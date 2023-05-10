b443c3bd2048c91f434eccd8d4c4c1f99c8ba69a
Shawn Smith <sds.smith24@gmail.com>
2023-05-10 15:43:21 -0400

    comment out unused imports

--- src/Components/PortfolioCard.jsx
+++ src/Components/PortfolioCard.jsx
@@ -1,16 +1,16 @@
-import { useContext } from 'react';
-import {Link as RouterLink} from 'react-router-dom';
+// import { useContext } from 'react';
+// import {Link as RouterLink} from 'react-router-dom';
 import { Link } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { ProjectContext } from '../context/ProjectContext';
+// import { ProjectContext } from '../context/ProjectContext';
 
 function PortfolioCard({project}) {
-    const {selectProject} = useContext(ProjectContext);
+    // const {selectProject} = useContext(ProjectContext);
 
     return (
         <div>
